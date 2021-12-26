b8c7d1de25d040e9465cefbf57388bde52e9bf33
Selim CAMUR <scamur54@gmail.com>
2021-12-26 20:28:41 +0100

    SelimBranch work is done

---
+++ src/test/java/SelimWork/SelimWork.java
@@ -0,0 +1,7 @@
+package SelimWork;
+
+public class SelimWork {
+    public static void main(String[] args) {
+        System.out.println("Doing first day work on Caner project, Selim Class");
+    }
+}
